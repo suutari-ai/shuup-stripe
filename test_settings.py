9c1a74f477a457312470e8f5cacc698a1ed868e7
Panu Tulimäki <panu@shuup.com>
2016-09-27 01:31:44 +0300

    Updates towards Shuup 0.5.0 and Django 1.9+
    Add migration for default managers and fix tests
    
    Refs SH-91

--- test_settings.py
+++ test_settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = (
     "shuup.front",
     "shuup.default_tax",
     "shuup_stripe",
+    "shuup.testing",
 )
 
 DATABASES = {
@@ -39,6 +40,23 @@ class DisableMigrations(object):
 
 MIGRATION_MODULES = DisableMigrations()
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "var", "media")
+
+LANGUAGES = [
+    ('en', 'English'),
+    ('fi', 'Finnish'),
+    ('ja', 'Japanese'),
+    ('zh-hans', 'Simplified Chinese'),
+    ('pt-br', 'Portuguese (Brazil)'),
+]
+
+PARLER_DEFAULT_LANGUAGE_CODE = "en"
+
+PARLER_LANGUAGES = {
+    None: [{"code": c, "name": n} for (c, n) in LANGUAGES],
+    'default': {
+        'hide_untranslated': False,
+    }
+}
 
 TEMPLATES = [
     {
