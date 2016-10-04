19e3fbbbedf42126adef9cf4adce5f8b573beeaa
Gurch Rai <gurch@shuup.com>
2016-10-03 20:08:09 -0700

    Add Stripe wizard form part
    Refs SH-326

--- shuup_stripe/admin_forms.py
+++ shuup_stripe/admin_forms.py
@@ -4,9 +4,12 @@
 #
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
-
 from django import forms
+from django.conf import settings
+
 from shuup.admin.forms import ShuupAdminForm
+from shuup.admin.modules.service_providers.forms import (ServiceWizardForm,
+                                                         ServiceWizardFormDef)
 
 from .models import StripeCheckoutPaymentProcessor
 
@@ -19,3 +22,22 @@ class StripeCheckoutAdminForm(ShuupAdminForm):
             'secret_key': forms.PasswordInput(render_value=True),
             'publishable_key': forms.PasswordInput(render_value=True),
         }
+
+
+class StripeCheckoutWizardForm(ServiceWizardForm):
+    class Meta:
+        model = StripeCheckoutPaymentProcessor
+        fields = ("name", "secret_key", "publishable_key")
+        widgets = {
+            'secret_key': forms.PasswordInput(render_value=True),
+            'publishable_key': forms.PasswordInput(render_value=True),
+        }
+
+
+class StripeCheckoutWizardFormDef(ServiceWizardFormDef):
+    def __init__(self):
+        super(StripeCheckoutWizardFormDef, self).__init__(
+            name="stripe",
+            form_class=StripeCheckoutWizardForm,
+            template_name="shuup/stripe/wizard_form.jinja"
+        )
