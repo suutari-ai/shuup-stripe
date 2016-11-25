9978735810717e72d9ea5c2d5fc14d8c15e0c75a
Panu Tulimäki <panu@shuup.com>
2016-11-24 19:43:51 -0800

    Initialize wizard form if needed

--- shuup_stripe/admin_forms.py
+++ shuup_stripe/admin_forms.py
@@ -5,11 +5,14 @@
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
 from django import forms
-from django.conf import settings
 
 from shuup.admin.forms import ShuupAdminForm
-from shuup.admin.modules.service_providers.forms import (ServiceWizardForm,
-                                                         ServiceWizardFormDef)
+from shuup.admin.modules.service_providers.wizard_form_defs import (
+    ServiceWizardFormDef
+)
+from shuup.admin.modules.service_providers.wizard_forms import (
+    ServiceWizardForm
+)
 
 from .models import StripeCheckoutPaymentProcessor
 
@@ -27,11 +30,22 @@ class StripeCheckoutAdminForm(ShuupAdminForm):
 class StripeCheckoutWizardForm(ServiceWizardForm):
     class Meta:
         model = StripeCheckoutPaymentProcessor
-        fields = ("name", "secret_key", "publishable_key")
+        fields = ("name", "service_name", "secret_key", "publishable_key")
         widgets = {
             'secret_key': forms.PasswordInput(render_value=True),
             'publishable_key': forms.PasswordInput(render_value=True),
         }
+
+    def __init__(self, **kwargs):
+        super(StripeCheckoutWizardForm, self).__init__(**kwargs)
+        if not self.provider:
+            return
+        service = self.get_payment_method()
+        if not service:
+            return
+        self.fields["service_name"].initial = service.name
+        self.fields["secret_key"].initial = self.provider.secret_key
+        self.fields["publishable_key"].initial = self.provider.publishable_key
 
 
 class StripeCheckoutWizardFormDef(ServiceWizardFormDef):
